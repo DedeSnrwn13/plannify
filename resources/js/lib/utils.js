ecf3f3c3ac89f25e3d162dff64f0e17f85c6e150
DedeSnrwn13 <sunarwandede13@gmail.com>
2025-01-19 11:51:45 +0700

    feat: configuration setup

--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -4,3 +4,19 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
     return twMerge(clsx(inputs))
 }
+
+export const STATUS = {
+    TODO: 'To do',
+    INPROGRESS: 'In Progress',
+    ONREVIEW: 'On Review',
+    DONE: 'Done',
+    UNKNOWN: 'Unknown',
+};
+
+export const PRIORITY = {
+    URGERN: 'Urgent',
+    HIGH: 'High',
+    MEDIUM: 'Medium',
+    LOW: 'Low',
+    UNKNOWN: 'Unknown',
+};
